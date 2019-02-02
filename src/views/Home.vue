e8abe4fd56e3ebf4d3a9979fd23b2fd8e2456aaf
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-02 04:27:16 +0300

    Use vue-material components for form. Write new styles. Formetting code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,65 +1,82 @@
 <template>
-  <div class="home">
-    <form @submit.prevent="sendData">
-      <div class="form-control">
-        <label for="name">Name</label>
-        <input v-model="name" id="name" type="text">
-      </div>
-      <div class="form-control">
-        <label for="email">Email</label>
-        <input v-model="email" id="email" type="email">
-      </div>
-      <div class="form-control">
-        <label for="address">Address</label>
-        <input v-model="address" id="address" type="text">
-      </div>
-      <div class="form-control">
-        <label>
-          <span>Gender</span>
-          <span>Male
-            <input v-model="gender" id="male" type="radio" value="male">
-          </span>
-          <span>Female
-            <input v-model="gender" id="female" type="radio" value="female">
-          </span>
-        </label>
-      </div>
-      <input type="submit" id="send" value="Send">
-    </form>
-  </div>
+    <div class="home">
+        <form @submit.prevent="sendData" id="main-form">
+
+            <md-input-container class="form-control">
+                <label>Name</label>
+                <md-input v-model="name" id="name" type="text" />
+            </md-input-container>
+
+            <md-input-container class="form-control">
+                <label>Email</label>
+                <md-input v-model="email" id="email" type="email" />
+            </md-input-container>
+
+            <md-input-container class="form-control">
+                <label>Address</label>
+                <md-input v-model="address" id="address" type="text" />
+            </md-input-container>
+
+            <div class="form-control">
+                <label>Gender</label>
+                <md-radio v-model="gender" id="male" md-value="male" class="md-primary">Male</md-radio>
+                <md-radio v-model="gender" id="female" md-value="female" class="md-primary">Female</md-radio>
+            </div>
+            <md-button type="summit" class="md-raised md-primary" id="send">Send</md-button>
+
+        </form>
+    </div>
 </template>
 
 
 <script>
-export default {
-  name: 'home',
-  data: function () {
-    return {
-      name: '',
-      email: '',
-      address: '',
-      gender: ''
-    };
-  },
-  methods: {
-    sendData() {
-      console.log(this.name, this.email, this.address, this.gender);
+    export default {
+        name: 'home',
+        data: function () {
+            return {
+                name: '',
+                email: '',
+                address: '',
+                gender: ''
+            };
+            },
+        methods: {
+            sendData() {
+                console.log(this.name, this.email, this.address, this.gender);
+            }
+        }
     }
-  }
-}
 </script>
 
 
 <style>
-  .form-control {
-    padding: 5px;
-  }
+    .form-control {
+        padding: 5px;
+    }
 
-  .form-control label {
-    display: block;
-  }
+    .form-control label {
+        display: block;
+    }
 
-  #send {
-    margin: 5px;
-  }
+    .form-control:nth-child(4) {
+        padding-left: 0;
+    }
+
+    .form-control:nth-child(4) > label {
+        font-size: 17px;
+        color: rgba(0, 0, 0, 0.6);
+    }
+
+    #send {
+        margin: 5px;
+    }
+
+    #main-form {
+        width: 30%;
+        min-width: 250px;
+        border: 1px solid lightgray;
+        border-radius: 8px;
+        padding: 5px;
+        margin: 10px auto;
+    }
 </style>
