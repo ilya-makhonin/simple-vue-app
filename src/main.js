f566c854824d0aa8f00ed7a39960c04a75ce2fb1
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-02 04:25:50 +0300

    Import vue-material and vue-material-css. Used Vue.use for connecting to vue-material

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueMaterial);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
