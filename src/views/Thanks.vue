7de2550c0dbbf5109452790ac341e00a50973cf9
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-02 04:28:01 +0300

    Create beta-version of the thanks page

--- src/views/Thanks.vue
+++ src/views/Thanks.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="thanks">
-    <h1>This is an about page</h1>
+      <md-card>
+          <md-card-media>
+              <img src="../assets/logo.png" alt="Vue-Logo" class="logo-redirect">
+          </md-card-media>
+          <md-card-content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea nostrum.
+          </md-card-content>
+          <md-card-actions>
+              <md-button class="md-primary">
+                  <router-link to="/">Home</router-link>
+              </md-button>
+          </md-card-actions>
+      </md-card>
   </div>
 </template>
+
+<style>
+    .thanks {
+        width: 25%;
+        min-width: 200px;
+        margin: 10px auto;
+    }
+</style>
