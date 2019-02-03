7ea02aa8ad85bf285c7053b50e5383ff32e94da5
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-04 02:31:56 +0300

    Updated code. Added styles, and changed markup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
     <div id="app">
-        <router-view/>
+        <header>
+            <div class="logo">
+                <img src="./assets/logo.png" alt="Logo-Vue" class="logo-vue">
+            </div>
+        </header>
+        <main>
+            <router-view/>
+        </main>
+
     </div>
 </template>
 
 <style>
+    #app {
+        background: url("./assets/black-background_00313351.jpg");
+        height: 100vh;
+    }
 
+    #app > header {
+        width: 100%;
+        max-height: 100px;
+        background-color: rgba(0, 0, 0, 0.6);
+        position: relative;
+        margin: 0;
+        padding: 5px 0;
+        z-index: 100;
+    }
+
+    .logo {
+        width: 70px;
+        margin: 0 auto;
+    }
 </style>
