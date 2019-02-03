8290bf3d39a9c534238dd5f8634965a92e11198b
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-03 23:33:42 +0300

    Created func for sending data to server. Added md-card component. Updated styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,45 @@
 <template>
     <div class="home">
-        <form @submit.prevent="sendData" id="main-form">
+        <md-card>
+            <form @submit.prevent="sendData" id="main-form">
+                <md-input-container class="form-control">
+                    <label>Name</label>
+                    <md-input v-model="name" id="name" type="text" />
+                </md-input-container>
 
-            <md-input-container class="form-control">
-                <label>Name</label>
-                <md-input v-model="name" id="name" type="text" />
-            </md-input-container>
+                <md-input-container class="form-control">
+                    <label>Email</label>
+                    <md-input v-model="email" id="email" type="email" />
+                </md-input-container>
 
-            <md-input-container class="form-control">
-                <label>Email</label>
-                <md-input v-model="email" id="email" type="email" />
-            </md-input-container>
+                <md-input-container class="form-control">
+                    <label>Address</label>
+                    <md-input v-model="address" id="address" type="text" />
+                </md-input-container>
 
-            <md-input-container class="form-control">
-                <label>Address</label>
-                <md-input v-model="address" id="address" type="text" />
-            </md-input-container>
-
-            <div class="form-control">
-                <label>Gender</label>
-                <md-radio v-model="gender" id="male" md-value="male" class="md-primary">Male</md-radio>
-                <md-radio v-model="gender" id="female" md-value="female" class="md-primary">Female</md-radio>
-            </div>
-            <md-button type="summit" class="md-raised md-primary" id="send">Send</md-button>
-
-        </form>
+                <div class="form-control">
+                    <label>Gender</label>
+                    <br>
+                    <md-radio v-model="gender" id="male" md-value="male" class="md-primary">Male</md-radio>
+                    <md-radio v-model="gender" id="female" md-value="female" class="md-primary">Female</md-radio>
+                </div>
+                <md-button type="summit" class="md-raised md-primary" id="send">Send</md-button>
+            </form>
+        </md-card>
     </div>
 </template>
 
 
 <script>
+    import axios from 'axios';
+    import MdCard from "../../node_modules/vue-material/src/components/mdCard/mdCard.vue";
+    import MdCardActions from "../../node_modules/vue-material/src/components/mdCard/mdCardActions.vue";
+
+
     export default {
+        components: {
+            MdCardActions,
+            MdCard},
         name: 'home',
         data: function () {
             return {
@@ -39,10 +48,21 @@
                 address: '',
                 gender: ''
             };
-            },
+        },
         methods: {
-            sendData() {
-                console.log(this.name, this.email, this.address, this.gender);
+            async sendData() {
+                await axios({
+                    url: 'http://localhost:3000/api/records',
+                    method: 'post',
+                    data: {
+                        name: this.name,
+                        email: this.email,
+                        address: this.address,
+                        gender: this.gender
+                    }
+                });
+
+                this.$router.push('thanks');
             }
         }
     }
@@ -50,14 +70,6 @@
 
 
 <style>
-    .form-control {
-        padding: 5px;
-    }
-
-    .form-control label {
-        display: block;
-    }
-
     .form-control:nth-child(4) {
         padding-left: 0;
     }
@@ -68,15 +80,16 @@
     }
 
     #send {
-        margin: 5px;
+        margin: 5px auto;
     }
 
     #main-form {
+        padding: 5px;
+    }
+
+    .home {
         width: 30%;
         min-width: 250px;
-        border: 1px solid lightgray;
-        border-radius: 8px;
-        padding: 5px;
-        margin: 10px auto;
+        margin: 30px auto;
     }
 </style>
