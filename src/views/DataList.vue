fdc2f937eefc7c50ab7849143076cb9e20bccdfd
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-05 23:38:25 +0300

    Created component for getting data from DB

---
+++ src/views/DataList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="data-list">
+        <button @click="getDataList">Get Data</button>
+        <div class="list-wrapper">
+            <div v-for="dataUser in dataList">
+                <p>{{ dataUser }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+      name: 'data',
+      data: function () {
+        return {
+          dataList: []
+        };
+      },
+      methods: {
+         async getDataList() {
+          try {
+            let response = await axios.get('http://localhost:3000/api/records');
+            console.log(response);
+            this.dataList = response.data;
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
