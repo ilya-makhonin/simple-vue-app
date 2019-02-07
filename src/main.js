c3b1b464ad3f995798fe842974c2ad0d2766238e
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-07 23:01:17 +0300

    Add TODO comments

--- src/main.js
+++ src/main.js
@@ -12,3 +12,7 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app');
+
+// TODO: Add pages for deleting and updating data in DB
+// TODO: A component for deleting will call "ClearDBCollections"
+// TODO: A component for updating will call "UpdateDBData"
