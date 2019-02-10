ee6dc3e6ac00c6cfc6deba94fd2794a1c938d4f7
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-10 23:51:03 +0300

    Add the step where we create a form without matetial design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -119,3 +119,61 @@
         background-color: rgba(255, 255, 255, 0.6);
     }
 </style>
+
+<!--
+Step 1
+<template>
+<div class="home">
+    <form @submit.prevent="sendData">
+      <div class="form-control">
+        <label for="name">Name</label>
+        <input v-model="name" id="name" type="text">
+      </div>
+      <div class="form-control">
+        <label for="email">Email</label>
+        <input v-model="email" id="email" type="email">
+      </div>
+      <div class="form-control">
+        <label for="address">Address</label>
+        <input v-model="address" id="address" type="text">
+      </div>
+      <div class="form-control">
+        <label for="gender">Gender</label>
+        <span>Male <input v-model="gender" id="gender" type="radio" value="male"></span>
+        <span>Female <input v-model="gender" id="gender" type="radio" value="female"></span>
+      </div>
+      <input type="submit" class="send" value="Send">
+    </form>
+</div>
+</template>
+<script>
+export default {
+   name: 'home',
+   data: function () {
+     return {
+       name: '',
+       email: '',
+       address: '',
+       gender: '',
+     }
+   },
+   methods: {
+     sendData() {
+       console.log(this.name, this.email, this.address, this.gender);
+     }
+   }
+}
+</script>
+<style>
+  .form-control {
+    padding: 5px;
+  }
+  .form-control label {
+    display: block;
+  }
+  .send {
+    margin: 5px
+  }
+</style>
+
+-->
