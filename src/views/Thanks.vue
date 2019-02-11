3e2105a673441955e9490df9537026eb2ded67df
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-11 20:48:39 +0300

    Deleted extra code

--- src/views/Thanks.vue
+++ src/views/Thanks.vue
@@ -54,12 +54,3 @@
         background: rgba(255, 255, 255, 0.6);
     }
 </style>
-
-<!--
-Step 1
-<template>
-    <div class="thanks">
-       <h1>Thank you for your record!</h1>
-    </div>
-</template>
--->
