ac3ee79c6e7dd87c9aed89310b03936ca4145d7d
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-06 23:53:51 +0300

    Added router for component which shows user by id

--- src/router.js
+++ src/router.js
@@ -3,25 +3,34 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Thanks from './views/Thanks.vue'
 import DataList from './views/DataList.vue'
+import OneUser from './views/OneUser.vue'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/thanks',
-      name: 'thanks',
-      component: Thanks
-    },
-    {
-      path: '/data',
-      name: 'data',
-      component: DataList
-    }
+      {
+          path: '/',
+          name: 'home',
+          component: Home
+
+      },
+      {
+          path: '/thanks',
+          name: 'thanks',
+          component: Thanks
+
+      },
+      {
+          path: '/data',
+          name: 'data',
+          component: DataList
+
+      },
+      {
+          path: '/user/:id',
+          name: 'user',
+          component: OneUser
+      }
   ]
 })
