ef5ba01e3bb32e9501f3f6860df77820ba82f669
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-06 23:55:16 +0300

    Component for getting user by id

---
+++ src/views/OneUser.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="user">
+        <div>Пользователь {{ $route.params.id }}</div>
+        <button @click="getMyParam">Go</button>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'user',
+        data: function () {
+            return {
+                name: '',
+                email: '',
+                address: '',
+                gender: ''
+            };
+        },
+        methods: {
+            getMyParam() {
+                console.log(this.$route.params);
+            }
+        }
+    }
+</script>
+
+
+<style>
+
+</style>
