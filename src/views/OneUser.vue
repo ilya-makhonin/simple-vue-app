e3c9dc7f27ceb7a1eda181e7c9b953531007f55e
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-07 23:56:54 +0300

    Created component for showing info about a user by id (don't work update info method)

--- src/views/OneUser.vue
+++ src/views/OneUser.vue
@@ -1,25 +1,94 @@
 <template>
     <div class="user">
-        <div>Пользователь {{ $route.params.id }}</div>
-        <button @click="getMyParam">Go</button>
+        <router-link to="/">
+            <md-button class="md-raised md-primary">Home</md-button>
+        </router-link>
+        <div class="user-info">
+            <md-card md-with-hover>
+                <md-card-header>
+                    <md-avatar>
+                        <img :src="getAvatarLink" alt="People">
+                    </md-avatar>
+                    <div class="md-title">User: {{ name }}</div>
+                    <div class="md-subhead">User's email: {{ email }}</div>
+                </md-card-header>
+                <md-card-content>
+                    <md-list class="md-double-line">
+                        <md-list-item>
+                            <div class="md-list-text-container">
+                                <span>{{ address }}</span>
+                                <span>Work address</span>
+                            </div>
+                            <div class="md-list-text-container">
+                                <span>{{ gender }}</span>
+                                <span>Gender</span>
+                            </div>
+                        </md-list-item>
+                    </md-list>
+                </md-card-content>
+                <md-card-actions>
+                    <md-button class="md-raised md-primary">Update profile</md-button>
+                    <md-button class="md-raised md-primary" @click="deleteUserById">Delete account</md-button>
+                </md-card-actions>
+            </md-card>
+        </div>
     </div>
 </template>
 
 
 <script>
+    import axios from 'axios';
+    import avatar from '../avatar_random';
+
     export default {
         name: 'user',
         data: function () {
+            this.getUseryId();
             return {
-                name: '',
-                email: '',
-                address: '',
-                gender: ''
+                name: 'Loading...',
+                email: 'Loading...',
+                address: 'Loading...',
+                gender: 'Loading...'
             };
         },
+
+        computed: {
+            getAvatarLink() {
+                return `https://api.adorable.io/avatars/100/${avatar()}@adorable.io.png`
+            }
+        },
+
         methods: {
-            getMyParam() {
-                console.log(this.$route.params);
+            async getUseryId() {
+                try {
+                    let response = await axios.get(`http://localhost:3000/api/records/${this.$route.params.id}`);
+                    this.name = response.data.name;
+                    this.email = response.data.email;
+                    this.address = response.data.address;
+                    this.gender = response.data.gender;
+                } catch (error) {
+                    console.log('Shit!');
+                }
+            },
+            async deleteUserById() {
+                await axios({
+                    url: `http://localhost:3000/api/records/${this.$route.params.id}`,
+                    method: 'delete'
+                });
+                this.$router.push('home');
+            },
+            async updateUserById() {
+                // This method is not working
+                await axios({
+                    url: `http://localhost:3000/api/records/${this.$route.params.id}`,
+                    method: 'put',
+                    data: {
+                        name: this.name,
+                        email: this.email,
+                        address: this.address,
+                        gender: this.gender
+                    }
+                });
             }
         }
     }
@@ -27,5 +96,9 @@
 
 
 <style>
-
+    .user-info {
+        width: 40%;
+        min-width: 300px;
+        margin: 20px auto;
+    }
 </style>
